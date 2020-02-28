a6765dc9d0062acfe3f457698f84ba9f7b4015a9
Samiul Amin Shanto <samiul.amin@shohoz.net>
2020-02-28 18:38:37 +0600

    Oops, api doc missed. here it is

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-02-28T12:30:26.912Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
